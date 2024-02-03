a188580c1711839617fea261f4c3283ad1dacfc6
sidb7 <63531357+sidb7@users.noreply.github.com>
2024-02-03 13:10:27 +0530

    Delete taskmanager - Copy-20230318T181514Z-001/taskmanager - Copy directory

--- taskmanager - Copy-20230318T181514Z-001/taskmanager - Copy/src/main/java/com/taskmanager/taskmanager/SubjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.taskmanager.taskmanager;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface SubjectRepository extends CrudRepository<Subject,String> {
-    
-}
